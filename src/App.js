3afd5f3280d3cc65024ac461bf6d7a1fe540e3e2
reski-id <pesan.reski@gmail.com>
2022-10-16 13:06:51 +0700

    component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 import './App.css';
+
+import HelloWorld from "./HelloWorld";
 
 class App extends Component{
   render(){
     return(
-      <div className="warna">Hallo Semuanya</div>
+      // <div className="warna">Hallo Semuanya</div>
+      <div>
+        <HelloWorld/>
+        <HelloWorld/>
+        <HelloWorld/>
+        <HelloWorld/>
+      </div>
     )
   }
 }
---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React,  { Component } from "react";
+import './App.css';
+
+class HelloWorld extends Component{
+    render(){
+        return(
+            <div className="warna">Hallo from Component</div> 
+        )
+    }
+}
+
+export default HelloWorld;
